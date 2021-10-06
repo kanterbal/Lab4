b85312a1e7991efed8ce143b261891d66f11b912
Sergei Kanter <sdkanter@outlook.com>
2021-10-06 13:45:05 +0200

    Ex1 done

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -10,6 +10,22 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите целое число > 0: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            if (number > 0)
+            {
+                for (int i = 1; i < number+1; i++)
+                {
+                    sum += 2 * i - 1;
+                    Console.Write("{0} ", sum);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка!!!");
+            }
+            Console.ReadKey();
         }
     }
 }
