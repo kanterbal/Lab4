9f3169d39d557011af16f7fe7f3c705c4c770703
Sergei Kanter <sdkanter@outlook.com>
2021-10-06 13:56:01 +0200

    Ex2 done.

---
+++ Ex2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводите последовательность чисел");
+            int negNumber = 0, posNumber = 0, number = 0;
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number > 0)
+                {
+                    posNumber++;
+                }
+                else if (number < 0)
+
+                {
+                    negNumber++;
+                }
+            }
+            while (number != 0);
+            if (negNumber > posNumber)
+            {
+                Console.WriteLine("Отрицательных больше");
+            }
+            else if (posNumber > negNumber)
+            {
+                Console.WriteLine("Положительных больше");
+            }
+            else
+            {
+                Console.WriteLine("Отрицательных и положительных поровну");
+            }
+            Console.ReadKey();
+        }
+    }
+}
